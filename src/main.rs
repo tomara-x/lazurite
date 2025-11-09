36e3b661b2ca3819c58b395294dea37b0febb63f
tomara_x <414d59@gmail.com>
2025-11-09 10:03:52 +0200

    Revert "handle input better"
    that was a bad idea
    This reverts commit 0af7279f10f3d2a8558cb65506da5db06c56f405.

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,9 @@ fn main() {
     println!("// https://codeberg.org/tomara-x/lazurite");
     println!("// this machine kills fascists");
     let mut lapis = Lapis::new();
-    let mut input = String::new();
     loop {
-        if io::stdin().read_line(&mut input).is_ok() {
+        if let Ok(input) = io::read_to_string(io::stdin()) {
             lapis.eval(&input);
-            input.clear();
         }
         thread::sleep(Duration::from_nanos(1));
     }
