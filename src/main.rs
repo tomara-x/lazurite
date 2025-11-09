0af7279f10f3d2a8558cb65506da5db06c56f405
tomara_x <414d59@gmail.com>
2025-11-09 03:21:13 +0200

    handle input better

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,11 @@ fn main() {
     println!("// https://codeberg.org/tomara-x/lazurite");
     println!("// this machine kills fascists");
     let mut lapis = Lapis::new();
+    let mut input = String::new();
     loop {
-        if let Ok(input) = io::read_to_string(io::stdin()) {
+        if io::stdin().read_line(&mut input).is_ok() {
             lapis.eval(&input);
+            input.clear();
         }
         thread::sleep(Duration::from_nanos(1));
     }
